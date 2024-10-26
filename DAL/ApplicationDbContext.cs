720bafbbbd2dc4a6994f933cf9947f3073efbf3d
Celina Huseby Botten <celinabotten@gmail.com>
2024-10-26 17:30:43 +0200

    Moved "ApplicationDbContext" and "DBInit" to the DAL folder. Also changed the namespace from ".Models" to ".DAL"

---
+++ DAL/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MapYourMeal.Models;
+
+namespace MapYourMeal.DAL;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) // Constructor
+    {
+        //Database.EnsureCreated(); // Creates an empty database in case it does not exist
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Restaurant> Restaurants { get; set; }
+}
