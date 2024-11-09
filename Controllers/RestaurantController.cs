64f7fd88e495fe90a8f44619979dc17bf023884e
ingridk2 <ingridk2@hotmail.com>
2024-11-09 15:58:52 +0100

    Added logger and error handling to RestaurantController, SearchResultController and UserController

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -10,15 +10,18 @@ namespace MapYourMeal.Controllers;
 public class RestaurantController : Controller
     {
         private readonly IRestaurantRepository _restaurantRepository;
+        private readonly ILogger<RestaurantController> _logger;
 
-        public RestaurantController(IRestaurantRepository restaurantRepository)
+        public RestaurantController(IRestaurantRepository restaurantRepository, ILogger<RestaurantController> logger)
         {
             _restaurantRepository = restaurantRepository;
+            _logger = logger;
         }
 
         [HttpGet]
         public IActionResult GetAllRestaurants()
         {
+            _logger.LogError("[RestaurantRepository] restaurant list not found while executing _restaurantRepository.GetAll()");
             var restaurants = _restaurantRepository.GetAll();
             return Ok(restaurants);
         }
@@ -30,6 +33,7 @@ public class RestaurantController : Controller
             var restaurants = await _restaurantRepository.GetAll();
             if(restaurants == null)
             {
+                _logger.LogError("[RestaurantRepository] restaurant list not found while executing _restaurantRepository.GetAll()");
                 return NotFound("Restaurant list not found");
             }
             var restaurantViewModel = new RestaurantViewModel(restaurants, "Table");
@@ -41,6 +45,7 @@ public class RestaurantController : Controller
             var restaurant = await _restaurantRepository.GetItemById(restaurantId);
             if (restaurant == null)
             {
+                _logger.LogError("[RestaurantController] Restaurant not found when updating the RestaurantId {restaurantId:0000}", restaurantId);
                 return NotFound();
             }
             return View(restaurant);
@@ -65,6 +70,7 @@ public class RestaurantController : Controller
                 if (returnOk)
                     return RedirectToAction("Index", "SearchResult");
             }
+            _logger.LogWarning("[RestaurantController] Restaurant creation failed {@restaurant}", restaurant);
             return View(restaurant);
         }
 
@@ -76,6 +82,7 @@ public class RestaurantController : Controller
             var restaurant = await _restaurantRepository.GetItemById(RestaurantId);
             if(restaurant == null)
             {
+                _logger.LogError("[RestaurantController] Restaurant not found when updating the RestaurantId {RestaurantId:0000}", RestaurantId);
                 Console.WriteLine($"Received RestaurantId: {RestaurantId}");
                 return BadRequest("Restaurant not found for the RestaurantId");
             }
@@ -92,6 +99,7 @@ public class RestaurantController : Controller
                 await _restaurantRepository.Update(restaurant);
                 return RedirectToAction(nameof(Table));
             }
+            _logger.LogWarning("[RestaurantController] Restaurant update failed {@restaurant}", restaurant);
             return View(restaurant);
         }
 
@@ -102,6 +110,7 @@ public class RestaurantController : Controller
             var restaurant = await _restaurantRepository.GetItemById(RestaurantId);
             if(restaurant== null)
             {
+                _logger.LogError("[RestaurantController] Restaurant not found for the RestaurantId {RestaurantId:0000}", RestaurantId);
                 return BadRequest("Restaurant not found for the RestaurantId");
             }
             return View(restaurant);
@@ -115,6 +124,7 @@ public class RestaurantController : Controller
             var restaurant = await _restaurantRepository.Delete(RestaurantId);
             if(restaurant == null)
             {
+                _logger.LogError("[RestaurantController] Restaurant deletion failed for the RestaurantId {RestaurantId:0000}", RestaurantId);
                 return BadRequest("Restaurant deletion failed");
             }
             return RedirectToAction(nameof(Table));
