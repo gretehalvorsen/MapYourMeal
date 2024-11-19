d8adb3c86eecec16b4c1a468abce7442c4244b26
Celina Huseby Botten <celinabotten@gmail.com>
2024-11-19 10:57:36 +0100

    Removed unused code

--- Models/Restaurant.cs
+++ Models/Restaurant.cs
@@ -14,18 +14,7 @@ namespace MapYourMeal.Models
         public DateTime CreatedDate { get; set; } = DateTime.Now;
         public string? ImageUrl { get; set; } // Single image URL for search result (hardcoded images)
         public byte[]? ImageData { get; set; } // For new user images
-        public string? ImageType { get; set; }
-        public string? DisplayImage
-        {
-            get
-            {
-                if (ImageData != null)
-                {
-                    return $"/RestaurantController/GetImage/{RestaurantId}";
-                }
-                return ImageUrl;
-            }
-        }
+        public string? ImageType { get; set; } // Checking image type (png, jpeg)
 
         // List of images for use in detailed views
         public List<string>? Images { get; set; } = new List<string>();
