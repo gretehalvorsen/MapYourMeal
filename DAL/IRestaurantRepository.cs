f3f34da77e026639e541557880e13275855fdc90
Celina Huseby Botten <celinabotten@gmail.com>
2024-11-19 10:58:32 +0100

    Added ? to <Restaurant>?>

--- DAL/IRestaurantRepository.cs
+++ DAL/IRestaurantRepository.cs
@@ -3,7 +3,7 @@ using MapYourMeal.Models;
 namespace MapYourMeal.DAL;
 public interface IRestaurantRepository
 {
-    Task<IEnumerable<Restaurant>> GetAll();
+    Task<IEnumerable<Restaurant>?> GetAll();
     Task<Restaurant?> GetItemById(int RestaurantId);
     Task<bool> Create(Restaurant restaurant);
     Task Update(Restaurant restaurant);
