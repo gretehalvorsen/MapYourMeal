3cf6adf2ca4a245d0488a977543c2fc3785326e3
eirinko <eirinkorvald.ek@gmail.com>
2024-11-20 15:14:51 +0100

    Restaurants saved in the DB shows up on map for creating restaurant.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -12,13 +12,25 @@ $(function(){
     }
 });
 
-function initCreateMap(){
+async function initCreateMap(){
     var createRestaurantMap = L.map('createRestaurantMap');
     createRestaurantMap.setView([59.91, 10.75], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(createRestaurantMap);
+
+    var restaurants = await fetchRestaurants();
+    restaurants.forEach(restaurant => {
+        L.marker([restaurant.latitude, restaurant.longitude])
+            .addTo(createRestaurantMap)
+            .bindPopup(
+                `<div style="width: 100 px">
+                    <h4>${restaurant.restaurantName}</h4>
+                    <a href="Restaurant?restaurantId=${restaurant.restaurantId}" 
+                    class="btn btn-secondary" style="display: flex; background-color: #2a5b80; color: #fff; justify-content: center;">Vis mer</a>
+                </div>`);
+    });
 
     var marker;
     createRestaurantMap.on('click', function(e) {
@@ -42,7 +54,6 @@ async function initMap(){
     }).addTo(map);
 
     var restaurants = await fetchRestaurants();
-    console.log(restaurants);
     restaurants.forEach(restaurant => {
         L.marker([restaurant.latitude, restaurant.longitude])
             .addTo(map)
